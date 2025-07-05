cb7851dc028da8c537fd3247b81d863d1a7165f0
soogil <zmfflrql5@gmail.com>
2025-07-05 19:43:13 +0900

    작은 사이즈에서 폰트 사이즈 작게 나오는 현상 수정

--- lib/presentation/view/main_page.dart
+++ lib/presentation/view/main_page.dart
@@ -364,7 +364,7 @@ extension ResponsiveFontSize on double {
   double responsiveFontSize(BuildContext context, {double? minFontSize}) {
     final width = MediaQuery.of(context).size.width;
     if (width < 600 && minFontSize != null) {
-      return minFontSize.sp;
+      return minFontSize;
     }
     return this;
   }
