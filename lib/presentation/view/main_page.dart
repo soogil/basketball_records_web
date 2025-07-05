bb60c3846817c9c5197e9f42f94efb4fc19dda07
soogil <zmfflrql5@gmail.com>
2025-07-05 13:20:15 +0900

    Revert "작은화면에서 폰트 사이즈 작아지는 현상 수정"
    This reverts commit 3a5cbc8ed139e47ff6af1316deac7eafcb66a075.

--- lib/presentation/view/main_page.dart
+++ lib/presentation/view/main_page.dart
@@ -364,7 +364,7 @@ extension ResponsiveFontSize on double {
   double responsiveFontSize(BuildContext context, {double? minFontSize}) {
     final width = MediaQuery.of(context).size.width;
     if (width < 600 && minFontSize != null) {
-      return minFontSize;
+      return minFontSize.sp;
     }
     return this;
   }
